17182b835139dd70162d900001767b25bfce8dce
Lorenz Uhlig <98092139+LoQue90@users.noreply.github.com>
2024-08-21 01:12:55 +0200

    Rework Brew Controll
    remove brewdetection
    adds brewtimer without controlling pump/valve
    add manual flush as own function
    manual flush with own mashineState kManualFlush
    add manualFlush to display templates and shottimer
    rework brew() and backflush()
    
    bring brewcontrol setting onto website
    clean up MQTT Brew stuff
    
    adding case kInteger to assignMQTTParam() so backflushCycles could be changed thru MQTT
    bring display settings onto the website
    rename some scale vars to be more precice
    add weightBrewed to MQTT sensors and HA auto discovery
    
    rename Shot timer, manual flush timer separated
    a own function to check if brew timer should be visible or not, based on brew is running

--- src/display/displayTemplateUpright.h
+++ src/display/displayTemplateUpright.h
@@ -11,17 +11,23 @@
  * @brief Send data to display
  */
 void printScreen() {
-    // Show shot timer:
-    if (displayShottimer()) {
+
+    // Show fullscreen brew timer:
+    if (displayFullscreenBrewTimer()) {
+        // Display was updated, end here
+        return;
+    }
+
+    // Show fullscreen manual flush timer:
+    if (displayFullscreenManualFlushTimer()) {
         // Display was updated, end here
         return;
     }
 
     // If no specific machine state was printed, print default:
 
-    if (((machineState == kPidNormal || machineState == kBrewDetectionTrailing) || ((machineState == kBrew || machineState == kShotTimerAfterBrew) && FEATURE_SHOTTIMER == 0) ||
-         ((machineState == kPidDisabled) && FEATURE_PIDOFF_LOGO == 0)) &&
-            (brewSwitchState != kBrewSwitchFlushOff) ||
+    if ((machineState == kPidNormal || (machineState == kBrew && FEATURE_SHOTTIMER == 0) ||
+         ((machineState == kPidDisabled) && FEATURE_PIDOFF_LOGO == 0)) && (brewSwitchState != kManualFlush) ||
         (machineState == kWaterTankEmpty) || (machineState == kPidDisabled) || (machineState == kStandby) || (machineState == kSteam)) {
         if (!tempSensor->hasError()) {
             u8g2.clearBuffer();
@@ -39,13 +45,13 @@ void printScreen() {
             u8g2.print((char)176);
             u8g2.print("C");
 
-            // Draw heat bar
-            u8g2.drawLine(0, 124, 63, 124);
-            u8g2.drawLine(0, 124, 0, 127);
-            u8g2.drawLine(64, 124, 63, 127);
-            u8g2.drawLine(0, 127, 63, 127);
-            u8g2.drawLine(1, 125, (pidOutput / 16.13) + 1, 125);
-            u8g2.drawLine(1, 126, (pidOutput / 16.13) + 1, 126);
+        // Draw heat bar
+        u8g2.drawLine(0, 124, 63, 124);
+        u8g2.drawLine(0, 124, 0, 127);
+        u8g2.drawLine(64, 124, 63, 127);
+        u8g2.drawLine(0, 127, 63, 127);
+        u8g2.drawLine(1, 125, (pidOutput / 16.13) + 1, 125);
+        u8g2.drawLine(1, 126, (pidOutput / 16.13) + 1, 126);
 
             // Show the heating logo when we are in regular PID mode
             if (FEATURE_HEATINGLOGO > 0 && machineState == kPidNormal && (setpoint - temperature) > 0.3 && brewSwitchState != kBrewSwitchFlushOff) {
@@ -150,6 +156,42 @@ void printScreen() {
 
                 u8g2.print(" s");
             }
+
+            // Brew time
+#if (FEATURE_BREWSWITCH == 1)
+        if (featureBrewControl) {
+            // Show brew time
+            if (shouldDisplayBrewTimer()) {
+                u8g2.setCursor(1, 34);
+                u8g2.print(langstring_brew_rot_ur);
+                u8g2.print(timeBrewed / 1000, 0);
+                u8g2.print("/");
+                u8g2.print(totalBrewTime / 1000, 0);
+                u8g2.print(" s");
+            }
+
+            // Shown flush time
+            if (machineState == kManualFlush) {
+                u8g2.setDrawColor(0);
+                u8g2.drawBox(1, 34, 100, 15);
+                u8g2.setDrawColor(1);
+                u8g2.setCursor(1, 34);
+                u8g2.print(langstring_manual_flush_rot_ur);
+                u8g2.print(timeBrewed / 1000, 0);
+                u8g2.print(" s");
+            }
+        }
+        else {
+            // Show brew time with optocoupler
+            if (shouldDisplayBrewTimer()) {
+                u8g2.setCursor(1, 34);
+                u8g2.print(langstring_brew_rot_ur);
+                u8g2.print(timeBrewed / 1000, 0);
+                u8g2.print(" s");
+            }
+        }
+#endif
+
             // For status info
             u8g2.drawFrame(0, 0, 64, 12);
 
@@ -164,30 +206,16 @@ void printScreen() {
                     }
                 }
                 else {
-                    u8g2.drawXBMP(4, 2, 8, 8, Antenna_NOK_Icon);
-                    u8g2.setCursor(56, 2);
-                    u8g2.print("RC: ");
-                    u8g2.print(wifiReconnects);
-                }
-
-                if (FEATURE_MQTT == 1) {
-                    if (mqtt.connected() == 1) {
-                        u8g2.setCursor(24, 2);
-                        u8g2.setFont(u8g2_font_profont11_tf);
-                        u8g2.print("MQTT");
-                    }
-                    else {
-                        u8g2.setCursor(24, 2);
-                        u8g2.print("");
-                    }
+                    u8g2.setCursor(24, 2);
+                    u8g2.print("");
                 }
             }
-            else {
-                u8g2.setCursor(4, 1);
-                u8g2.print("Offline");
-            }
-
-            u8g2.sendBuffer();
         }
+        else {
+            u8g2.setCursor(4, 1);
+            u8g2.print("Offline");
+        }
+
+        u8g2.sendBuffer();
     }
 }
