3efbf390c8e158b7ee1e7ddf1e2c0b770b6cec92
Scott Condie <scottcondie24+github@gmail.com>
2025-05-28 19:12:21 +1000

    changed to silvia-dev added fake weights added fake pressure added fake temp

--- src/display/displayTemplateScale.h
+++ src/display/displayTemplateScale.h
@@ -12,6 +12,9 @@
  * @brief Send data to display
  */
 void printScreen() {
+    static float fakeWeight = 0.0;
+    scaleFailure = false;
+    static float weight = 22.3;
 
     // Show fullscreen brew timer:
     if (displayFullscreenBrewTimer()) {
@@ -78,6 +81,8 @@ void printScreen() {
     if (featureBrewControl) {
 
         if (shouldDisplayBrewTimer()) {
+            fakeWeight += 0.2;
+            currBrewWeight = fakeWeight;
             // time
             displayBrewTime(32, 36, langstring_brew, currBrewTime, totalTargetBrewTime);
 
@@ -86,6 +91,10 @@ void printScreen() {
             u8g2.drawBox(32, 27, 100, 10);
             u8g2.setDrawColor(1);
             displayBrewWeight(32, 26, currBrewWeight, targetBrewWeight, scaleFailure);
+        }
+        else {
+            fakeWeight = 0;
+            currReadingWeight = 10.3;
         }
         // Shown flush time while machine is flushing
         if (machineState == kManualFlush) {
