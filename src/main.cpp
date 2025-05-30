fe42580dad21321b28fac46b39fe6adc78f06c0f
Scott Condie <scottcondie24+github@gmail.com>
2025-05-31 07:51:06 +1000

    updated timing debugging

--- src/main.cpp
+++ src/main.cpp
@@ -158,6 +158,15 @@ bool websiteUpdateRunning = false;
 bool mqttUpdateRunning = false;
 bool hassioUpdateRunning = false;
 
+// Debugging - activity flags
+enum ActivityType : uint16_t {
+    ACT_DISPLAY_READY   = 1 << 0,  // 0x01
+    ACT_DISPLAY_RUNNING = 1 << 1,  // 0x02
+    ACT_WEBSITE_RUNNING = 1 << 2,  // 0x04
+    ACT_MQTT_RUNNING    = 1 << 3,  // 0x08
+    ACT_HASSIO_RUNNING  = 1 << 4   // 0x10
+};
+
 Switch* waterTankSensor;
 
 GPIOPin* statusLedPin;
@@ -208,9 +217,9 @@ int writeSysParamsToMQTT(bool continueOnError);
 void updateStandbyTimer(void);
 void resetStandbyTimer(void);
 void wiFiReset(void);
-void printLoopTimingsAsList(void);
-void printActivityTypeAsList(void);
 void debugTimingLoop(void);
+void printActivityFlags(const uint16_t* activity, int size);
+void printTimingAndActivityBatch(const unsigned long* timing, const uint16_t* activity, int size);
 
 //debug timings
 const int LOOP_HISTORY_SIZE = 20;
@@ -1773,69 +1782,88 @@ void loop() {
     debugTimingLoop();
 }
 
-void printLoopTimingsAsList() {
-  char buffer[512];  // Make sure this is large enough
+//print what has caused the long loop time
+void printActivityFlags(const uint16_t* activity, int size) {
+  char activityBuffer[512];
   int len = 0;
-  len += snprintf(buffer + len, sizeof(buffer) - len, "Loop timings (ms): [");
-  for (int i = 0; i < LOOP_HISTORY_SIZE; i++) {
-    len += snprintf(buffer + len, sizeof(buffer) - len, "%lu", loopTimings[i]);
-    if (i < LOOP_HISTORY_SIZE - 1) {
-      len += snprintf(buffer + len, sizeof(buffer) - len, ", ");
+
+  len += snprintf(activityBuffer + len, sizeof(activityBuffer) - len, "Activity (short): [");
+  for (int i = 0; i < size; ++i) {
+    // Convert flags to short notation
+    if (activity[i] == 0) {
+      len += snprintf(activityBuffer + len, sizeof(activityBuffer) - len, "_");
+    } else {
+      if (activity[i] & ACT_DISPLAY_READY)   len += snprintf(activityBuffer + len, sizeof(activityBuffer) - len, "r");
+      if (activity[i] & ACT_DISPLAY_RUNNING) len += snprintf(activityBuffer + len, sizeof(activityBuffer) - len, "D");
+      if (activity[i] & ACT_WEBSITE_RUNNING) len += snprintf(activityBuffer + len, sizeof(activityBuffer) - len, "W");
+      if (activity[i] & ACT_MQTT_RUNNING)    len += snprintf(activityBuffer + len, sizeof(activityBuffer) - len, "M");
+      if (activity[i] & ACT_HASSIO_RUNNING)  len += snprintf(activityBuffer + len, sizeof(activityBuffer) - len, "H");
     }
+    if (i < size - 1) len += snprintf(activityBuffer + len, sizeof(activityBuffer) - len, ",");
   }
-  len += snprintf(buffer + len, sizeof(buffer) - len, "]");
-  LOGF(DEBUG, "%s", buffer);
+  len += snprintf(activityBuffer + len, sizeof(activityBuffer) - len, "]");
+
+  LOGF(DEBUG, "%s", activityBuffer);
 }
 
-void printActivityTypeAsList() {
-  char buffer[512];  // Make sure this is large enough
-  int len = 0;
-  len += snprintf(buffer + len, sizeof(buffer) - len, "Activity Type: [");
-  for (int i = 0; i < LOOP_HISTORY_SIZE; i++) {
-    len += snprintf(buffer + len, sizeof(buffer) - len, "%lu", activityType[i]);
-    if (i < LOOP_HISTORY_SIZE - 1) {
-      len += snprintf(buffer + len, sizeof(buffer) - len, ", ");
-    }
+
+// Print both timing and compact activity in one batch
+void printTimingAndActivityBatch(const unsigned long* timing, const uint16_t* activity, int size) {
+  char timingBuffer[512];
+  int tLen = 0;
+
+  tLen += snprintf(timingBuffer + tLen, sizeof(timingBuffer) - tLen, "Loop timing (ms): [");
+  for (int i = 0; i < size; ++i) {
+    tLen += snprintf(timingBuffer + tLen, sizeof(timingBuffer) - tLen, "%lu", timing[i]);
+    if (i < size - 1) tLen += snprintf(timingBuffer + tLen, sizeof(timingBuffer) - tLen, ",");
   }
-  len += snprintf(buffer + len, sizeof(buffer) - len, "]");
-  LOGF(DEBUG, "%s", buffer);
+  tLen += snprintf(timingBuffer + tLen, sizeof(timingBuffer) - tLen, "]");
+
+  // Only two log lines total
+  LOGF(DEBUG, "%s", timingBuffer);
+  printActivityFlags(activity, size);
 }
 
+//Store all long duration activities and their loop times, send when array is full
 void debugTimingLoop() {
-    unsigned long loopDuration = millis() - previousMillisDebug;
-    previousMillisDebug = millis();
-    if((loopDuration > 35)||(displayUpdateRunning||websiteUpdateRunning||mqttUpdateRunning||hassioUpdateRunning)) {
-        if (loopDuration >= maxloop) {// && loopDuration < 100000) {
-            maxloop = loopDuration;
-        }
-        loopTimings[loopIndex] = loopDuration;
-        activityType[loopIndex] = 0;
-        if(displayBufferReady) activityType[loopIndex]+= 1;
-        if(displayUpdateRunning) activityType[loopIndex]+= 10;
-        if(websiteUpdateRunning) activityType[loopIndex]+= 100;
-        if(mqttUpdateRunning) activityType[loopIndex]+= 1000;
-        if(hassioUpdateRunning) activityType[loopIndex]+= 10000;
-        displayUpdateRunning = false;
-        websiteUpdateRunning = false;
-        mqttUpdateRunning = false;
-        hassioUpdateRunning = false;
+    static const int LOOP_HISTORY_SIZE = 20;
+    static unsigned long loopTiming[LOOP_HISTORY_SIZE];
+    static uint16_t activityType[LOOP_HISTORY_SIZE];
+    static unsigned long previousMillisDebug = millis();
+    static unsigned long lastSendMillisDebug = millis();
+    static unsigned int loopIndex = 0;
+    static unsigned int loopCount = 0;
+    static unsigned long maxLoop = 0;
 
-        loopIndex = (loopIndex + 1) % LOOP_HISTORY_SIZE;
-        if(loopIndex == 0){
-            printLoopTimingsAsList();
-            printActivityTypeAsList();
-            unsigned long reportTime = millis() - lastSendMillisDebug;
-            unsigned long maxResult = maxloop;
-            LOGF(DEBUG, "Max time %lu -- 20 entries report time %lu (ms)", maxResult, reportTime);
-            if(maxmqtt > 0) {
-                LOGF(DEBUG, "Max mqtt time %lu (us)", maxmqtt);
-                maxmqtt = 0;
+    IFLOG(DEBUG) {
+        loopCount += 1;
+        unsigned long loopDuration = millis() - previousMillisDebug;
+        previousMillisDebug = millis();
+        if((loopDuration > 35)||(displayUpdateRunning||websiteUpdateRunning||mqttUpdateRunning||hassioUpdateRunning)) {
+            if (loopDuration >= maxLoop) {
+                maxLoop = loopDuration;
             }
-            lastSendMillisDebug = millis();
-            maxloop = 0;
+            loopTiming[loopIndex] = loopDuration;
+            activityType[loopIndex] = 0;
+            if (displayBufferReady) activityType[loopIndex] |= ACT_DISPLAY_READY;
+            if (displayUpdateRunning) activityType[loopIndex] |= ACT_DISPLAY_RUNNING;
+            if (websiteUpdateRunning) activityType[loopIndex] |= ACT_WEBSITE_RUNNING;
+            if (mqttUpdateRunning) activityType[loopIndex] |= ACT_MQTT_RUNNING;
+            if (hassioUpdateRunning) activityType[loopIndex] |= ACT_HASSIO_RUNNING;
+
+            loopIndex = (loopIndex + 1) % LOOP_HISTORY_SIZE;
+            if(loopIndex == 0){
+                printTimingAndActivityBatch(loopTiming, activityType, LOOP_HISTORY_SIZE);
+                unsigned long reportTime = millis() - lastSendMillisDebug;
+                float avgLoopMs = loopCount > 0 ? ((float)reportTime / loopCount) : 0;
+                LOGF(DEBUG, "Max time %lu (ms) -- %i entries report time %lu (ms) -- average %0.2f (ms)", maxLoop, LOOP_HISTORY_SIZE, reportTime, avgLoopMs);
+                lastSendMillisDebug = millis();
+                loopCount = 0;
+            }
         }
     }
 }
+
 void looppid() {
     // Only do Wifi stuff, if Wifi is connected
     if (WiFi.status() == WL_CONNECTED && offlineMode == 0) {
