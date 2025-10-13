Barour/Kaffeemaschine

added analog pressure sensor through ads1115, added k-type thermocouple through MAX6675, added water LED, changed pinmapping removed TSIC and Honeywell pressure added second scale clock pin removed additional languages changed temp measurement to average of last 4 and increased sample rate

src/Config.h
@@ -353,6 +353,8 @@ class Config {
             _configDefs.emplace("hardware.leds.brew.inverted", ConfigDef::forBool(false));
             _configDefs.emplace("hardware.leds.steam.enabled", ConfigDef::forBool(false));
             _configDefs.emplace("hardware.leds.steam.inverted", ConfigDef::forBool(false));
+            _configDefs.emplace("hardware.leds.water.enabled", ConfigDef::forBool(false));
+            _configDefs.emplace("hardware.leds.water.inverted", ConfigDef::forBool(false));
 
             // Hardware - Sensors
             _configDefs.emplace("hardware.sensors.temperature.type", ConfigDef::forInt(0, 0, 1));

src/ParameterRegistry.cpp
@@ -1016,6 +1016,24 @@ void ParameterRegistry::initialize(Config& config) {
         nullptr,
         "Invert the steam LED logic (for common anode LEDs)"
     );
+    
+    addBoolConfigParam(
+        "hardware.leds.water.enabled",
+        "Enable Water LED",
+        sHardwareLedSection,
+        2331,
+        nullptr,
+        "Enable water indicator LED"
+    );
+
+    addBoolConfigParam(
+        "hardware.leds.water.inverted",
+        "Invert Water LED",
+        sHardwareLedSection,
+        2332,
+        nullptr,
+        "Invert the water LED logic (for common anode LEDs)"
+    );
 
     // Sensors
     addEnumConfigParam(
@@ -1024,7 +1042,7 @@ void ParameterRegistry::initialize(Config& config) {
         sHardwareSensorSection,
         2401,
         nullptr,
-        (const char* const[]){"TSIC306", "Dallas DS18B20"},
+        (const char* const[]){"MAX6675", "Dallas DS18B20"},
         2,
         "Type of temperature sensor connected"
     );

src/hardware/pinmapping.h
@@ -12,21 +12,26 @@
  */
 
 // Switches/Buttons
-#define PIN_POWERSWITCH 39
+#define PIN_POWERSWITCH 36 // not used
 #define PIN_BREWSWITCH  34
 #define PIN_STEAMSWITCH 35
-#define PIN_WATERSWITCH 36
+#define PIN_WATERSWITCH 39 // reusing poweswitch
 
-#define PIN_ROTARY_DT  4 // Rotary encoder data pin
-#define PIN_ROTARY_CLK 3 // Rotary encoder clock pin
-#define PIN_ROTARY_SW  5 // Rotary encoder switch
+#define PIN_ROTARY_DT  5   // Rotary encoder data pin
+#define PIN_ROTARY_CLK 4   // Rotary encoder clock pin
+#define PIN_ROTARY_SW  3   // Rotary encoder switch
 
 // Sensors
 #define PIN_TEMPSENSOR      16
 #define PIN_WATERTANKSENSOR 23
-#define PIN_HXDAT           32 // Brew scale data pin 1
+#define PIN_HXDAT           33 // Brew scale data pin 1
 #define PIN_HXDAT2          25 // Brew scale data pin 2
-#define PIN_HXCLK           33 // Brew scale clock pin
+#define PIN_HXCLK           23 // Brew scale clock pin 1
+#define PIN_HXCLK2          32 // Brew scale clock pin 2
+// Pin mapping for MAX6675 temperature sensor
+#define PIN_TEMPERATURE_SO  12
+#define PIN_TEMPERATURE_CS  13
+#define PIN_TEMPERATURE_CLK 16 // this can be the same as PIN_TEMPSENSOR as it wouldnt otherwise be used
 
 /**
  * Output pins
@@ -38,9 +43,10 @@
 #define PIN_HEATER 2
 
 // LEDs
-#define PIN_STATUSLED 26
+#define PIN_STATUSLED 15 // not connected
 #define PIN_BREWLED   19
 #define PIN_STEAMLED  1
+#define PIN_WATERLED  26 // reusing statusled pin
 
 // Periphery
 #define PIN_ZC 18 // Dimmer circuit Zero Crossing

scottcondie24/clevercoffee-master

src/Config.h
@@ -355,6 +355,8 @@ class Config {
             _configDefs.emplace("hardware.leds.brew.inverted", ConfigDef::forBool(false));
             _configDefs.emplace("hardware.leds.steam.enabled", ConfigDef::forBool(false));
             _configDefs.emplace("hardware.leds.steam.inverted", ConfigDef::forBool(false));
+            _configDefs.emplace("hardware.leds.hot_water.enabled", ConfigDef::forBool(false));
+            _configDefs.emplace("hardware.leds.hot_water.inverted", ConfigDef::forBool(false));
 
             // Hardware - Sensors
             _configDefs.emplace("hardware.sensors.temperature.type", ConfigDef::forInt(0, 0, 1));

src/ParameterRegistry.cpp
@@ -1048,6 +1048,24 @@ void ParameterRegistry::initialize(Config& config) {
         nullptr,
         "Invert the steam LED logic (for common anode LEDs)"
     );
+    
+    addBoolConfigParam(
+        "hardware.leds.hot_water.enabled",
+        "Enable Water LED",
+        sHardwareLedSection,
+        2331,
+        nullptr,
+        "Enable water indicator LED"
+    );
+
+    addBoolConfigParam(
+        "hardware.leds.hot_water.inverted",
+        "Invert Water LED",
+        sHardwareLedSection,
+        2332,
+        nullptr,
+        "Invert the water LED logic (for common anode LEDs)"
+    );
 
     // Sensors
     addEnumConfigParam(
@@ -1056,7 +1074,7 @@ void ParameterRegistry::initialize(Config& config) {
         sHardwareSensorSection,
         2401,
         nullptr,
-        (const char* const[]){"TSIC306", "Dallas DS18B20"},
+        (const char* const[]){"MAX6675", "Dallas DS18B20"},
         2,
         "Type of temperature sensor connected"
     );

src/hardware/pinmapping.h
@@ -12,21 +12,26 @@
  */
 
 // Switches/Buttons
-#define PIN_POWERSWITCH 39
+#define PIN_POWERSWITCH 36 // not used
 #define PIN_BREWSWITCH  34
 #define PIN_STEAMSWITCH 35
-#define PIN_WATERSWITCH 36
+#define PIN_WATERSWITCH 39 // reusing poweswitch
 
-#define PIN_ROTARY_DT  4 // Rotary encoder data pin
-#define PIN_ROTARY_CLK 3 // Rotary encoder clock pin
-#define PIN_ROTARY_SW  5 // Rotary encoder switch
+#define PIN_ROTARY_DT  5   // Rotary encoder data pin
+#define PIN_ROTARY_CLK 4   // Rotary encoder clock pin
+#define PIN_ROTARY_SW  3   // Rotary encoder switch
 
 // Sensors
 #define PIN_TEMPSENSOR      16
 #define PIN_WATERTANKSENSOR 23
-#define PIN_HXDAT           32 // Brew scale data pin 1
+#define PIN_HXDAT           33 // Brew scale data pin 1
 #define PIN_HXDAT2          25 // Brew scale data pin 2
-#define PIN_HXCLK           33 // Brew scale clock pin
+#define PIN_HXCLK           23 // Brew scale clock pin 1
+#define PIN_HXCLK2          32 // Brew scale clock pin 2
+// Pin mapping for MAX6675 temperature sensor
+#define PIN_TEMPERATURE_SO  12
+#define PIN_TEMPERATURE_CS  13
+#define PIN_TEMPERATURE_CLK 16 // this can be the same as PIN_TEMPSENSOR as it wouldnt otherwise be used
 
 /**
  * Output pins
@@ -38,9 +43,10 @@
 #define PIN_HEATER 2
 
 // LEDs
-#define PIN_STATUSLED 26
-#define PIN_BREWLED   19
-#define PIN_STEAMLED  1
+#define PIN_STATUSLED   15 // not connected
+#define PIN_BREWLED     19
+#define PIN_STEAMLED    1
+#define PIN_HOTWATERLED 26 // reusing statusled pin
 
 // Periphery
 #define PIN_ZC 18 // Dimmer circuit Zero Crossing